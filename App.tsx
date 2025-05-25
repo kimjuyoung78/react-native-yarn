67828e7413b2efd81a1d55ab7e48654232dd9d86
Claire <claire@cntt.co.kr>
2025-05-26 03:09:47 +0900

    add : 기본 screen 추가

--- App.tsx
+++ App.tsx
@@ -1,27 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
 
 const App: React.FC = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
-      <Text style={styles.text}>React Native + TypeScript!</Text>
-    </View>
-  );
+  return <HomeScreen />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#CFFFE5',
-  },
-  text: {
-    fontSize: 24,
-    color: '#222',
-    fontWeight:'700',
-  },
-});
 
 export default App;
